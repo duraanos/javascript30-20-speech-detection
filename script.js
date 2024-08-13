27bbb91d297aaaf7ee4eca70af7c5b7172cf1867
duraanos <duran364@anadolu.edu.tr>
2024-08-13 11:37:39 +0300

    Create package.json file

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 'use strict';
 
+const words = document.querySelector('.words');
+let p = document.createElement('p');
+words.appendChild(p);
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+console.log(recognition);
+
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+recognition.addEventListener('results', function (e) {
+  console.log(e);
+  const transcript = [...e.results]
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+
+  if (e.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild('p');
+  }
+});
+
+// recognition.start();
+recognition.addEventListener('end', recognition.start);
